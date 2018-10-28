b34f7e8ffd148357de9ce69f8fba7caf84c51c9e
zhonghaoxi <zhonghaoxi@yeah.net>
2018-10-28 21:58:19 +0800

    add float32

--- reader/formatter/format.py
+++ reader/formatter/format.py
@@ -90,5 +90,5 @@ class AYPredictionFormatter:
 
         matrix = [ss[i] + title[i] + pjjg[i] for i in range(len(data))]
         matrix = torch.Tensor(matrix)
-        label = torch.from_numpy(np.array(label, dtype=np.int))
+        label = torch.from_numpy(np.array(label, dtype=np.float32))
         return {'input': matrix, 'label': label}
