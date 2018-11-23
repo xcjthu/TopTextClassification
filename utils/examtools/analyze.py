f1cffa017225b39683cd9a4b73d4cda6c60a8dbf
zhonghaoxi <zhonghaoxi@yeah.net>
2018-11-23 13:41:07 +0800

    gg

---
+++ utils/examtools/analyze.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+data_path = "/data/disk3/private/zhx/exam/data/origin_data"
+
+analyze_result = {
+    "total": 0,
+    "num": [0, 0, 0, 0, 0],
+    "statement_len": {},
+    "answer_len": {}
+}
+
+file_list = os.listdir(data_path)
+for filename in file_list:
+    file = open(os.path.join(data_path, filename), "r")
+    for line in file:
+        d = json.loads(line)
+        analyze_result["total"] += 1
+        analyze_result["num"][len(d["answer"])] += 1
+
+        l = len(d["statement"])
+        if not (l in analyze_result["statement_len"].keys()):
+            analyze_result["statement_len"][l] = 0
+        analyze_result["statement_len"][l] += 1
+
+        for option in d["option_list"]:
+            l = len(d["option_list"][option])
+            if not (l in analyze_result["answer_len"].keys()):
+                analyze_result["answer_len"][l] = 0
+            analyze_result["answer_len"][l] += 1
+
+json.dump(analyze_result, open("analyze_result.txt", "w"), indent=2, sort_keys=True, ensure_ascii=False)
