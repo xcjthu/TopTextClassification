5655d687d5607de5727532f9704450531776b4e0
zhonghaoxi <zhonghaoxi@yeah.net>
2018-12-03 11:43:51 +0800

    are you ready?

--- model/get_model.py
+++ model/get_model.py
@@ -1,11 +1,13 @@
 from model.model.LSTM import LSTM
 from model.model.TextCNN import TextCNN
 from model.model.SFKS.BasicCNN import BasicCNN
+from model.model.SFKS.co_matching import CoMatching
 
 model_list = {
     "LSTM": LSTM,
     "TextCNN": TextCNN,
-    "SFKSCNN": BasicCNN
+    "SFKSCNN": BasicCNN,
+    "Comatching": CoMatching
 }
 
 
