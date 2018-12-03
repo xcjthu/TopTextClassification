59adff672e47edeae586ea1300f8bc5b82140a3e
zhonghaoxi <zhonghaoxi@yeah.net>
2018-12-03 10:45:35 +0800

    gg

---
+++ utils/examtools/merge.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+path = "/data/disk3/private/zhx/exam/data/origin_data/format"
+work_list = ["train", "test"]
+
+if __name__ == "__main__":
+    for work in work_list:
+        for b in range(0, 2):
+            data = []
+            for a in range(1, 5):
+                f = open(os.path.join(path, "%d_%d_%s.json" % (a, b, work)), "r")
+                for line in f:
+                    data.append(json.loads(line))
+
+            f = open(os.path.join(path, "%d_%s.json" % (b, work)), "w")
+            for d in data:
+                print(json.dumps(d, ensure_ascii=False, sort_keys=True), file=f)
