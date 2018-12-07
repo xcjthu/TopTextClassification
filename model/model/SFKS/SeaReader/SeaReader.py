dd3d80124554ed611c519b16a156b94cef692ddb
xcjthu <xiaocj16@mails.tsinghua.edu.cn>
2018-12-07 11:08:01 +0800

    seareader

--- model/model/SFKS/SeaReader/SeaReader.py
+++ model/model/SFKS/SeaReader/SeaReader.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable as Var
-
+from utils.util import calc_accuracy, gen_result
 
 class Gate(nn.Module):
     def __init__(self, config, input_size):
@@ -12,11 +12,12 @@ class Gate(nn.Module):
         self.input_size = input_size
 
     def forward(self, seq, gate_control):
-        gate = self.gate_w(seq.view(-1, seq.shape[-1]))
+        gate_control = gate_control.contiguous()
+        gate = self.gate_w(gate_control.view(-1, gate_control.shape[-1]))
         gate = F.sigmoid(gate)
-        gate = gate.view(seq.shape[0],-1, 1)
-	
-        return torch.bmm(gate, seq)
+        gate = gate.view(seq.shape[0],-1, 1).expand(seq.shape[0], seq.shape[1], seq.shape[2])
+
+        return seq.mul(gate)
 
 
 class SeaReader(nn.Module):
@@ -40,7 +41,7 @@ class SeaReader(nn.Module):
         # self.doc_reason_gate = Gate(config, usegpu, self.hidden_size * 2)
         self.intergrate_gate = nn.Linear(2 * self.hidden_size, 1)
         
-        self.output_layer = nn.Linear(4 * self.hidden_size, 4)
+        self.output_layer = nn.Linear(4 * self.hidden_size, 1)
 
     def init_multi_gpu(self, device):
         pass
@@ -49,7 +50,7 @@ class SeaReader(nn.Module):
         
         if (usegpu):
             self.context_statement_hidden = Var(torch.Tensor(1, self.batch_size * 4, self.hidden_size).cuda())
-            self.context_doc_hidden = Var(torch.Tensor(1, self.batch_size * self.topN, self.hidden_size).cuda())
+            self.context_doc_hidden = Var(torch.Tensor(1, self.batch_size * self.topN * 4, self.hidden_size).cuda())
             self.doc_reason_hidden = Var(torch.Tensor(1, self.batch_size, self.hidden_size).cuda())
             self.statement_reason_hidden = Var(torch.Tensor(1, self.batch_size, self.hidden_size).cuda())
         
@@ -63,9 +64,11 @@ class SeaReader(nn.Module):
         labels = data['label']
         # documents shape : batchsize * topn * doclength * vecsize
         
+        '''
         print("question:", question.shape)
         print("option:", option_list.shape)
         print('labels:', labels.shape)
+        '''
 
         # 将问题描述和选项拼接在一起
         quetmp = torch.cat([question.unsqueeze(1) for i in range(4)], dim = 1)
@@ -74,17 +77,13 @@ class SeaReader(nn.Module):
 	
         # cal matching Matrix (有一定的可能性这个写法有点问题)
         # statmp: batch_size * 4 * topn, self.statement_len + self.option_len, self.hidden_size
-        print("statement:", statement.shape)
-        print("hidden", self.context_statement_hidden.shape)
         statmp, self.context_statement_hidden = self.context_layer(statement, self.context_statement_hidden)
         statmp = torch.cat([statmp.unsqueeze(1) for i in range(self.topN)], dim = 1).view(self.batch_size * 4 * self.topN, self.statement_len + self.option_len, self.hidden_size)
-        # statmp, self.context_statement_hidden = self.context_layer(statmp, self.context_statement_hidden)
 
 
-        # docstmp: batch_szie * 4 * topn, self.doc_len, self.hidden_size 
-        doctmp, self.context_doc_hidden = self.context_layer(documents, self.context_doc_hidden)
-        docstmp = torch.cat([documents.unsqueeze(1) for i in range(4)], dim = 1).view(self.batch_size * 4 * self.topN, self.doc_len, self.vecsize)
-        # docstmp, self.context_doc_hidden = self.context_layer(docstmp, self.context_doc_hidden)
+        # docstmp: batch_szie * 4 * topn, self.doc_len, self.hidden_size
+        # print('document shape:', documents.shape)
+        docstmp, self.context_doc_hidden = self.context_layer(documents.view(self.batch_size * self.topN * 4, self.doc_len, self.vecsize), self.context_doc_hidden)
 
         # match_m: batch_size * 4 * topn, self.statement_len + self.option_len, self.doc_len
         match_m = torch.bmm(statmp, torch.transpose(docstmp, 1, 2)) # .view(self.batch_size, 4, self.topN, self.statement_len + self.option_len, self.doc_len)
@@ -105,7 +104,7 @@ class SeaReader(nn.Module):
         # batch_size * 4 * topn, 
         doc_match = torch.bmm(doc_read, torch.transpose(doc_read, 1, 2))
         softmax = F.softmax(doc_match, dim = 2)
-        doc_read = torch.bmm(softmax, doc_read).view(-1, self.topN * self.doc_len, 2 * self.hidden_size)
+        doc_read = torch.bmm(softmax, doc_read).view(-1, self.doc_len, 2 * self.hidden_size)
 
 
         # get matching feature
@@ -120,11 +119,13 @@ class SeaReader(nn.Module):
         statement = self.reason_gate.forward(read_summary, statmp)
         documents = self.reason_gate.forward(doc_read, docstmp)
 
-        statement, self.statement_reason_hidden = self.statement_reason_layer(statement, self.statement_reason_hidden)
+        statement, self.statement_reason_hidden = self.statement_reason_layer(statement)
         documents, self.doc_reason_hidden = self.doc_reason_layer(documents)
+
 
         statement = torch.max(statement, dim = 1)[0]
         documents = torch.max(documents, dim = 1)[0]
+        
 
         documents = torch.cat([statement, documents], dim = 1)
 
@@ -148,7 +149,5 @@ class SeaReader(nn.Module):
 
 
 
-
-		
 
 
