2a035a4a536451c5ed51d23b373631a8349d5580
zhonghaoxi <zhonghaoxi@yeah.net>
2019-03-15 17:45:55 +0800

    gg

---
+++ utils/examtools/crawl.py
@@ -0,0 +1,43 @@
+import json
+import os
+import urllib
+import urllib3
+import requests
+
+http = urllib3.PoolManager()
+
+output_path = "crawl_data/"
+os.makedirs(output_path, exist_ok=True)
+
+sessions = requests.session()
+sessions.headers[
+    'User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
+
+
+def crawl(word):
+    url = "https://baike.baidu.com/item/%s" % (urllib.parse.urlencode({"": word})[1:])
+    # print(url)
+
+    res = http.request("GET", url)
+
+    s = bytes.decode(res.data)
+
+    print(s, file=open(os.path.join(output_path, "%s.html" % word), "w", encoding="utf8"))
+
+    if s.find("百度百科错误页") != -1:
+        return False
+
+
+if __name__ == "__main__":
+    word_list = json.load(open("/data/disk3/private/zhx/exam/data/dict/final_dict.txt", "r"))
+
+    for word in word_list:
+        if crawl(word):
+            pass
+        else:
+            print(word)
+            
+        import random
+
+        if random.randint(1, 20) == 1:
+            gg
