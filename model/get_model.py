dd7f4d219a57161ff9b072805683154e1b8d267d
zhonghaoxi <zhonghaoxi@yeah.net>
2019-04-01 21:10:25 +0800

    ?

--- model/get_model.py
+++ model/get_model.py
@@ -3,7 +3,7 @@ from model.model.TextCNN import TextCNN
 from model.model.SFKS.BasicCNN import BasicCNN
 from model.model.SFKS.co_matching import CoMatching
 from model.model.SFKS.SeaReader.SeaReader import SeaReader
-from model.model.SFKS.co_matching import CoMatching, CoMatching2, CoMatching3
+from model.model.SFKS.co_matching import CoMatching, CoMatching2, CoMatching3, CoMatching4
 from model.model.SFKS.multi_matching import MultiMatchNet
 from model.model.SFKS.conv_spatial_att import ConvSpatialAtt
 from model.model.SFKS.bert import SFKSBert
@@ -21,6 +21,7 @@ model_list = {
     "SeaReader": SeaReader,
     "Comatching2": CoMatching2,
     "Comatching3": CoMatching3,
+    "Comatching4": CoMatching4,
     "MultiMatch": MultiMatchNet,
     "ConvSpatialAtt": ConvSpatialAtt,
     "Bert": Bert,
