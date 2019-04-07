06a168f7dc745a1cea28fcf29c46c76569111f4d
zhonghaoxi <zhonghaoxi@yeah.net>
2019-04-07 10:04:27 +0800

    testing

--- utils/examtools/label_web/web.py
+++ utils/examtools/label_web/web.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template
 import os
 import json
 
+from elastic.elastic import search_doc
+
 app = Flask(__name__, static_folder=os.path.join(os.getcwd(), "static"),
             static_url_path='/static',
             template_folder=os.path.join(os.getcwd(), "templates"))
@@ -11,56 +13,36 @@ if os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)), "loc
 else:
     import config as config
 
-data = []
-data2 = []
 
-
-def init_question():
-    global data, data2
-    data = json.load(open("/data/disk3/private/zhx/exam/data/solve/x.json", "r"))
-    data2 = json.load(open("/home/zhx/final_biao.json", "r"))
-
-
-@app.route("/xcj")
-def xcj():
-    global data
+@app.route("/search")
+def search():
+    text = ""
     result = []
-    option = ""
-    num = ""
-    if "option" in request.args.keys() and "num" in request.args.keys():
-        option = request.args["option"]
-        num = request.args["num"]
-        if num == "":
-            pass
-        else:
-            num = int(request.args["num"])
-            for a in range(0, 18):
-                result.append(data2[num]["reference"][option][a])
-    for a in range(0, len(result)):
-        result[a] = str(a + 1) + ". " + result[a]
-    return render_template("main.html", result=result, option=option, num=num)
+    if "text" in request.args:
+        body = {
+            "query": {
+                "bool": {
+                    "should": [
+                        {
+                            "match": {
+                                "text": request.args["text"]
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+
+        data = search_doc("content", "data", body, size=20)
+        for x in data["hits"]:
+            result.append({"name": x["_source"]["text"], x["_source"]["id"]})
+    return render_template("search.html", result=result, text=text)
 
 
-@app.route("/")
-def root():
-    global data
-    result = []
-    option = ""
-    num = ""
-    if "option" in request.args.keys() and "num" in request.args.keys():
-        option = request.args["option"]
-        num = request.args["num"]
-        if num == "":
-            pass
-        else:
-            num = int(request.args["num"])
-            for a in range(0, 18):
-                result.append(data[num]["reference"][option][a])
-    for a in range(0, len(result)):
-        result[a] = str(a + 1) + ". " + result[a]
-    return render_template("main.html", result=result, option=option, num=num)
+@app.route("/view")
+def voew():
+    return render_template("view.html")
 
 
 if __name__ == "__main__":
-    init_question()
     app.run(host=config.host, port=config.port, debug=config.debug, threaded=True)
