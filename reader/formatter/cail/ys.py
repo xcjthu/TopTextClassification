6d7be2dcd92a86c91e6ecbf292f3bd85a7296efb
zhonghaoxi <zhonghaoxi@yeah.net>
2019-06-24 16:15:31 +0800

    ?

--- reader/formatter/cail/ys.py
+++ reader/formatter/cail/ys.py
@@ -55,6 +55,6 @@ class YSBertFormatter:
             label.append(temp)
 
         input = torch.LongTensor(input)
-        label = torch.LongStorage(label)
+        label = torch.LongTensor(label)
 
         return {'input': input, 'label': label}
